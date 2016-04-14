95a1bc0e58e9bb18f8d29ef1d10b3e2fe7a83e16
Eli Green <eli@zigr.org>
2016-04-14 12:52:54 +0200

    add "hello world" from libnds examples

--- src/main.c
+++ src/main.c
@@ -1,4 +1,34 @@
-int main(int argc, char **argv)
+#include <nds.h>
+#include <stdio.h>
+
+volatile int frame = 0;
+
+void Vblank()
 {
+    frame++;
+}
+
+int main(void)
+{
+    touchPosition touchXY;
+
+    irqSet(IRQ_VBLANK, Vblank);
+
+    consoleDemoInit();
+
+    iprintf("        @rasky\n");
+ 
+    while(1) {
+    
+        swiWaitForVBlank();
+        touchRead(&touchXY);
+
+        // print at using ansi escape sequence \x1b[line;columnH 
+        iprintf("\x1b[10;0HFrame = %d", frame);
+        iprintf("\x1b[16;0HTouch x = %04X, %04X\n", touchXY.rawx, touchXY.px);
+        iprintf("Touch y = %04X, %04X\n", touchXY.rawy, touchXY.py);        
+    
+    }
+
     return 0;
 }
