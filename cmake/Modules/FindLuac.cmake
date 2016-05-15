94299e429248e318a161388092b98dfbaadc2b7a
Giovanni Bajo <rasky@develer.com>
2016-05-16 00:57:27 +0200

    Bye bye cmake

--- cmake/Modules/FindLuac.cmake
+++
@@ -1,9 +0,0 @@
-find_program(LUAC_EXE NAMES luac luac5.3)
-
-if(NOT LUAC_EXE)
-  message(FATAL_ERROR "Cannot find luac")
-else()
-  message(STATUS "Found luac -- ${LUAC_EXE}")
-endif()
-
-set(LUA_FOUND 1)
