cc1e0a77807f92ecbd1faca61ca85d2d9bb1e2fe
Giovanni Bajo <rasky@develer.com>
2016-05-21 23:39:52 +0200

    Disable debugBreakpoint() on ARM7 (not available)

--- src/debug.h
+++ src/debug.h
@@ -9,5 +9,7 @@ int debugf(const char* format, ...);
 // Prints a value as a binary string, with the given label prefix.
 int debugb(const char* label, u32 val);
 
+#ifdef ARM9
 // Trigger a breakpoint in the emulator
 void debugBreakpoint();
+#endif
