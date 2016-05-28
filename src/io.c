067b71a0db95a28adbe97393b9015c97f7da528d
Lorenzo Villani <lorenzo@villani.me>
2016-05-28 17:11:56 +0200

    Add slurp: prefix to logging calls

--- src/io.c
+++ src/io.c
@@ -8,16 +8,16 @@ Bytes slurp(const char* path)
 {
     FILE* fp = fopen(path, "rb");
     if (fseek(fp, 0, SEEK_END) != 0) {
-        debugf("Error seeking to end of in %s\n", path);
+        debugf("slurp: error seeking to end of in %s\n", path);
     }
 
     long size = ftell(fp);
     if (size == -1L) {
-        debugf("Error getting size of %s\n", path);
+        debugf("slurp: error getting size of %s\n", path);
     }
 
     if (fseek(fp, 0, SEEK_SET) != 0) {
-        debugf("Error seeking back to 0 in %s\n", path);
+        debugf("slurp: error seeking back to 0 in %s\n", path);
     }
 
     Bytes blob;
@@ -25,7 +25,7 @@ Bytes slurp(const char* path)
     blob.data = (u8*)malloc(size);
 
     if (!blob.data) {
-        debugf("Error allocating %i bytes to read %s with\n", size, path);
+        debugf("slurp: error allocating %i bytes to read %s with\n", size, path);
     }
 
     size_t read = fread(blob.data, sizeof(u8), size, fp);
@@ -33,7 +33,7 @@ Bytes slurp(const char* path)
         debugf("slurp: Expected %i bytes, got %i from %s\n", size, read, path);
     }
 
-    debugf("Read %i from %s\n", read, path);
+    debugf("slurp: read %i from %s\n", read, path);
 
     return blob;
 }
